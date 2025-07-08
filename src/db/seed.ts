eccaa72d4dc7a0e06daf1554fdfc600e61bf117a
Eduardo Maia <edumaia909@gmail.com>
2025-07-08 14:45:43 -0300

    feat: add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,17 +3,22 @@ import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
 
 await reset(db, schema)
+
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
-        description: f.loremIpsum()
+        description: f.loremIpsum(),
       },
+    },
+    questions: {
+      count: 20,
     },
   }
 })
+
 await sql.end()
 
 // biome-ignore lint/suspicious/noConsole: only used in dev
